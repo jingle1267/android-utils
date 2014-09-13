1fae3a42b160188beac634f32eb46716dc35d571
jingle1267 <jingle1267@163.com>
2014-09-13 22:40:57 +0800

    [NewFeature] Add network util.

--- src/com/worthed/utils/NetUtil.java
+++ src/com/worthed/utils/NetUtil.java
@@ -120,6 +120,23 @@ public class NetUtil {
 		}
 		return false;
 	}
+	
+	/**
+	 * 检测当前开打的网络类型是否4G
+	 * @param context
+	 * @return
+	 */
+	public static boolean is4G(Context context) {
+		ConnectivityManager connectivityManager = (ConnectivityManager) context
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo activeNetInfo = connectivityManager.getActiveNetworkInfo();
+		if (activeNetInfo != null && activeNetInfo.isConnectedOrConnecting()) {
+			if (activeNetInfo.getType() == TelephonyManager.NETWORK_TYPE_LTE) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 	/**
 	 * 只是判断WIFI
@@ -127,7 +144,7 @@ public class NetUtil {
 	 * @param context
 	 * @return
 	 */
-	public static boolean isNetworkAvailableWiFi(Context context) {
+	public static boolean isWiFi(Context context) {
 		ConnectivityManager manager = (ConnectivityManager) context
 				.getSystemService(Context.CONNECTIVITY_SERVICE);
 		State wifi = manager.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
@@ -168,4 +185,63 @@ public class NetUtil {
 		return num;
 	}
 
+	/**
+	 * 枚举网络状态 NET_NO：没有网络 NET_2G:2g网络 NET_3G：3g网络 NET_4G：4g网络 NET_WIFI：wifi
+	 * NET_UNKNOWN：未知网络
+	 */
+	public static enum NetState {
+		NET_NO, NET_2G, NET_3G, NET_4G, NET_WIFI, NET_UNKNOWN
+	};
+	
+	/**
+	 * 判断当前是否网络连接
+	 * 
+	 * @param context
+	 * @return 状态码
+	 */
+	public NetState isConnected(Context context) {
+		NetState stateCode = NetState.NET_NO;
+		ConnectivityManager cm = (ConnectivityManager) context
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo ni = cm.getActiveNetworkInfo();
+		if (ni != null && ni.isConnectedOrConnecting()) {
+			switch (ni.getType()) {
+			case ConnectivityManager.TYPE_WIFI:
+				stateCode = NetState.NET_WIFI;
+				break;
+			case ConnectivityManager.TYPE_MOBILE:
+				switch (ni.getSubtype()) {
+				case TelephonyManager.NETWORK_TYPE_GPRS: // 联通2g
+				case TelephonyManager.NETWORK_TYPE_CDMA: // 电信2g
+				case TelephonyManager.NETWORK_TYPE_EDGE: // 移动2g
+				case TelephonyManager.NETWORK_TYPE_1xRTT:
+				case TelephonyManager.NETWORK_TYPE_IDEN:
+					stateCode = NetState.NET_2G;
+					break;
+				case TelephonyManager.NETWORK_TYPE_EVDO_A: // 电信3g
+				case TelephonyManager.NETWORK_TYPE_UMTS:
+				case TelephonyManager.NETWORK_TYPE_EVDO_0:
+				case TelephonyManager.NETWORK_TYPE_HSDPA:
+				case TelephonyManager.NETWORK_TYPE_HSUPA:
+				case TelephonyManager.NETWORK_TYPE_HSPA:
+				case TelephonyManager.NETWORK_TYPE_EVDO_B:
+				case TelephonyManager.NETWORK_TYPE_EHRPD:
+				case TelephonyManager.NETWORK_TYPE_HSPAP:
+					stateCode = NetState.NET_3G;
+					break;
+				case TelephonyManager.NETWORK_TYPE_LTE:
+					stateCode = NetState.NET_4G;
+					break;
+				default:
+					stateCode = NetState.NET_UNKNOWN;
+				}
+				break;
+			default:
+				stateCode = NetState.NET_UNKNOWN;
+			}
+
+		}
+		return stateCode;
+	}
+    
 }
