f3ed3848873daa469bb5c828e1e67476506ab6ba
Zhenguo <jingle1267@163.com>
2014-12-30 18:22:26 +0800

    [NewFeature] Add getRuntimeValue() method.

--- src/com/worthed/util/AppUtils.java
+++ src/com/worthed/util/AppUtils.java
@@ -18,6 +18,8 @@ package com.worthed.util;
 import java.io.File;
 
 import java.io.FileFilter;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.security.MessageDigest;
 import java.util.ArrayList;
 import java.util.List;
@@ -379,7 +381,7 @@ public class AppUtils {
         PackageManager pManager = context.getPackageManager();
         List<PackageInfo> paklist = pManager.getInstalledPackages(0);
         for (int i = 0; i < paklist.size(); i++) {
-            PackageInfo pak = (PackageInfo) paklist.get(i);
+            PackageInfo pak = paklist.get(i);
             if ((pak.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) <= 0) {
                 // customs applications
                 apps.add(pak);
@@ -395,6 +397,67 @@ public class AppUtils {
      */
     public static int getSDKVersion() {
         return android.os.Build.VERSION.SDK_INT;
+    }
+
+    /**
+     * 是否Dalvik模式
+     *
+     * @return 结果
+     */
+    public static boolean isDalvik() {
+        return "Dalvik".equals(getCurrentRuntimeValue());
+    }
+
+    /**
+     * 是否ART模式
+     *
+     * @return 结果
+     */
+    public static boolean isART() {
+        String currentRuntime = getCurrentRuntimeValue();
+        return "ART".equals(currentRuntime) || "ART debug build".equals(currentRuntime);
+    }
+
+    /**
+     * 获取手机当前的Runtime
+     *
+     * @return 正常情况下可能取值Dalvik, ART, ART debug build;
+     */
+    public static String getCurrentRuntimeValue() {
+        try {
+            Class<?> systemProperties = Class.forName("android.os.SystemProperties");
+            try {
+                Method get = systemProperties.getMethod("get",
+                        String.class, String.class);
+                if (get == null) {
+                    return "WTF?!";
+                }
+                try {
+                    final String value = (String) get.invoke(
+                            systemProperties, "persist.sys.dalvik.vm.lib",
+                        /* Assuming default is */"Dalvik");
+                    if ("libdvm.so".equals(value)) {
+                        return "Dalvik";
+                    } else if ("libart.so".equals(value)) {
+                        return "ART";
+                    } else if ("libartd.so".equals(value)) {
+                        return "ART debug build";
+                    }
+
+                    return value;
+                } catch (IllegalAccessException e) {
+                    return "IllegalAccessException";
+                } catch (IllegalArgumentException e) {
+                    return "IllegalArgumentException";
+                } catch (InvocationTargetException e) {
+                    return "InvocationTargetException";
+                }
+            } catch (NoSuchMethodException e) {
+                return "SystemProperties.get(String key, String def) method is not found";
+            }
+        } catch (ClassNotFoundException e) {
+            return "SystemProperties class is not found";
+        }
     }
 
 }
