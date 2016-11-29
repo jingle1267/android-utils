257a4635fa192a7e9d7b9ea83f09987a09a201eb
Zhenguo <jingle1267@163.com>
2016-11-29 18:56:15 +0800

    [DEL] 删除无用代码

--- app/src/main/java/com/ihongqiqu/util/DateUtils.java
+++ app/src/main/java/com/ihongqiqu/util/DateUtils.java
@@ -35,7 +35,6 @@ public final class DateUtils {
     public static final String yyyyMMDD = "yyyy-MM-dd";
     public static final String yyyyMMddHHmmss = "yyyy-MM-dd HH:mm:ss";
     public static final String HHmmss = "HH:mm:ss";
-    public static final String hhmmss = "HH:mm:ss";
     public static final String LOCALE_DATE_FORMAT = "yyyy年M月d日 HH:mm:ss";
     public static final String DB_DATA_FORMAT = "yyyy-MM-DD HH:mm:ss";
     public static final String NEWS_ITEM_DATE_FORMAT = "hh:mm M月d日 yyyy";
